fb0756e622b083969ce153559ce030c2c57ac3fb
Xevion <xevioni@yandex.com>
2020-05-05 10:00:40 -0500

    move random sampling method into separate class, general cleanup, update .gitignore with less

---
+++ procedural-placement/Assets/PointGeneration.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PointGeneration {
+    public static List<Vector2> random_sampling(int numPoints, Vector2 range, Vector2 offset) {
+        // Create a new empty list of points, and add some randomly
+        List<Vector2> points = new List<Vector2>();
+        for (int i = 0; i < numPoints; i++) {
+            points.Add(
+                new Vector2(
+                    Random.Range(-range.x, range.x),
+                    Random.Range(-range.y, range.y)));
+        }
+
+        return points;
+    }
+}
